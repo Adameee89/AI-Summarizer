afb7f0996d4e621bc3feae205b88da6bfa74e761
Adam Vegvari <brokenskijas9@gmail.com>
2024-06-27 15:11:58 +0200

    Website/Arcticle Summerizer with rapid api

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          800: '#2d2d2d',
+          900: '#1f1f1f',
+        },
+        orange: {
+          500: '#f97316',
+        },
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        fadeInDown: {
+          '0%': { opacity: 0, transform: 'translateY(-10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-out',
+        fadeInDown: 'fadeInDown 1s ease-out',
+        fadeInUp: 'fadeInUp 1s ease-out',
+      },
+    },
+  },
+  plugins: [],
+}
+
